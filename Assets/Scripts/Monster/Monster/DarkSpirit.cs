a49a4c0c33831d5492fdaec5795713870b4c0882
shane5828 <72241385+shane5828@users.noreply.github.com>
2024-11-18 01:37:06 +0900

    11.17 간단작업..
    어둠정령 대충 해놨는데 지금 완성할거임

--- Assets/Scripts/Monster/Monster/DarkSpirit.cs
+++ Assets/Scripts/Monster/Monster/DarkSpirit.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class DarkSpirit : MonoBehaviour
 {
-     // ╬ф го╠Б╫х╢ы
+    private Animator animator;
+    private Transform skilltowerTransform;
+
+     void Start()
+    {
+        animator = GetComponent<Animator>();
+        GameObject skilltowerObject = GameObject.FindWithTag("skilltower");
+        if (castleObject != null)
+        {
+            skilltowerTransform = skilltowerObject.transform;
+        }
+    }
 }
