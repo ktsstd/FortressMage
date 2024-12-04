4709de4b8fb0ec211d5906916c45da33cfd02dcf
shane5828 <kph5828@naver.com>
2024-12-05 06:32:41 +0900

    12.05 1차 ^-^
    기억안남 뭐했더라

---
+++ Assets/Scripts/Monster/Additional/BossSkill5.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSkill5 : MonoBehaviour
+{
+    void Start()
+    {
+        // GameObject[] playerObj =         
+    }
+}
