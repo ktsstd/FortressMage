abc98b17b48c1331ebacb7a148227201380fe293
shane5828 <72241385+shane5828@users.noreply.github.com>
2024-12-05 13:19:59 +0900

    12.05 5차
    그 그거 그 5번 테스트

--- Assets/Scripts/Monster/Additional/BossSkill5.cs
+++ Assets/Scripts/Monster/Additional/BossSkill5.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class BossSkill5 : MonoBehaviourPun
+public class BossSkill5 : MonoBehaviour
 {
     private Vector3 defaultPos;
+    private PhotonView pv;
+
     void Start()
     {
+        pv = GetComponent<PhotonView>();
         defaultPos = transform.position;
-        GameObject[] playertag = GameObject.FindGameObjectsWithTag("Player");
-        foreach(GameObject playerObj in playertag)
+        pv.RPC("MoveDefault", RpcTarget.All);
+    }
+
+    [PunRPC]
+    public void MoveDefault()
+    {
+        if (PhotonNetwork.IsMasterClient)
         {
-            playerObj.transform.position = defaultPos;
+            GameObject[] playertag = GameObject.FindGameObjectsWithTag("Player");
+            foreach(GameObject playerObj in playertag)
+            {
+                playerObj.transform.position = defaultPos;
+                PlayerController playerScript = playerObj.GetComponent<PlayerController>();
+                playerScript.OnPlayerStun(2f);
+                PhotonNetwork.Instantiate("Additional/Boss_Skill_5_1", defaultPos, Quaternion.identity); // Quaternion.Euler(-90, 0, 0)
+            }
         }
     }
 }
